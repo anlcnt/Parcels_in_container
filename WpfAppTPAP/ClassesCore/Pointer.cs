db55a2ce569aca0ef31e37a269e5fe9bfd7547a7
Djovenga <djovas@rambler.ru>
2020-09-30 19:12:53 +0300

    new class Pointer

---
+++ WpfAppTPAP/ClassesCore/Pointer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppTPAP.ClassesCore
+{
+    class Pointer
+    {
+        public Pointer()
+        {
+            MinX = 0;
+            MinY = 0;
+            MaxX = 0;
+            MaxY = 0;
+        }
+        public int MinY { get; set; }
+        public int MinX { get; set; }
+        public int MaxY { get; set; }
+        public int MaxX { get; set; }
+    }
+}
