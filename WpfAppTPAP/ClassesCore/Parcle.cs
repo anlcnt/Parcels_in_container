5020917319d9ebae8bec6d6085a22829d9269703
Igor Shmelev <shmelev@ttechnika.ru>
2020-09-30 13:20:43 +0300

    Добавил класс посылки и контейнера

---
+++ WpfAppTPAP/ClassesCore/Parcle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppTPAP.ClassesCore
+{
+    class Parcle
+    {
+        public Parcle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public Parcle(int width)
+        {
+            Width = width;
+            Height = width;
+        }
+
+        public int Width { get; }
+        public int Height { get; }
+    }
+}
